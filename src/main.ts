30c38efb6723fa94acc803a778ec476ab7d6d854
Cees Voesenek <cjvoesenek@proton.me>
2025-07-02 21:32:12 +0200

    Add simple URL strip application

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,14 @@
-console.log("Hello, world!");
+import { stripUrl } from "./lib/strip";
+import "./main.css";
+
+const original = document.getElementById("original") as HTMLTextAreaElement;
+const cleaned = document.getElementById("cleaned") as HTMLTextAreaElement;
+
+original.addEventListener("input", () => {
+  try {
+    const cleanedUrl = stripUrl(original.value);
+    cleaned.value = cleanedUrl.toString();
+  } catch {
+    cleaned.value = "";
+  }
+});
