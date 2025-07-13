52b52b44a484c161429c3e350420f054776a5df3
Cees Voesenek <cjvoesenek@proton.me>
2025-07-13 14:39:12 +0200

    Select entire text field upon focus

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,22 @@ import "./main.css";
 const original = document.getElementById("original") as HTMLTextAreaElement;
 const cleaned = document.getElementById("cleaned") as HTMLTextAreaElement;
 
-original.addEventListener("input", () => {
-  try {
-    const cleanedUrl = stripUrl(original.value);
-    cleaned.value = cleanedUrl.toString();
-  } catch {
-    cleaned.value = "";
-  }
-});
+addUrlChangeListener();
+
+addSelectAllListener(original);
+addSelectAllListener(cleaned);
+
+function addUrlChangeListener(): void {
+  original.addEventListener("input", () => {
+    try {
+      const cleanedUrl = stripUrl(original.value);
+      cleaned.value = cleanedUrl.toString();
+    } catch {
+      cleaned.value = "";
+    }
+  });
+}
+
+function addSelectAllListener(element: HTMLTextAreaElement): void {
+  element.addEventListener("focus", () => element.select());
+}
