769d56420f67183c81e95fef562927f71194231b
Cees Voesenek <cjvoesenek@proton.me>
2025-07-13 15:03:33 +0200

    Add clear and copy buttons

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,17 @@ import "./main.css";
 const original = document.getElementById("original") as HTMLTextAreaElement;
 const cleaned = document.getElementById("cleaned") as HTMLTextAreaElement;
 
+const clearButton = document.getElementById("clear") as HTMLButtonElement;
+const copyButton = document.getElementById("copy") as HTMLButtonElement;
+const copiedText = document.getElementById("copied") as HTMLSpanElement;
+
 addUrlChangeListener();
 
 addSelectAllListener(original);
 addSelectAllListener(cleaned);
+
+addClearListener();
+addCopyListener();
 
 function addUrlChangeListener(): void {
   original.addEventListener("input", () => {
@@ -22,4 +29,31 @@ function addUrlChangeListener(): void {
 
 function addSelectAllListener(element: HTMLTextAreaElement): void {
   element.addEventListener("focus", () => element.select());
+}
+
+function addClearListener(): void {
+  clearButton.addEventListener("click", () => {
+    original.value = "";
+    cleaned.value = "";
+  });
+}
+
+let timeoutHandle: ReturnType<typeof window.setTimeout> | null = null;
+function addCopyListener(): void {
+  copyButton.addEventListener("click", () => {
+    navigator.clipboard
+      .writeText(cleaned.value)
+      .then(() => {
+        copiedText.classList.add("visible");
+
+        if (timeoutHandle) window.clearTimeout(timeoutHandle);
+        timeoutHandle = window.setTimeout(() => {
+          copiedText.classList.remove("visible");
+          timeoutHandle = null;
+        }, 2000);
+      })
+      .catch((error) =>
+        console.error(`Failed to write URL to clipboard: ${error}`),
+      );
+  });
 }
